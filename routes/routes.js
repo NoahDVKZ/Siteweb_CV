4f623f97499e1a0999472bc98f3be2920308c646
Noah Della Valle <noah.dellavalle@student.hepl.be>
2026-05-16 00:39:08 +0200

    html in root

--- routes/routes.js
+++ routes/routes.js
@@ -7,16 +7,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/html/main.html'));
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 router.get('/experience', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/html/experience.html'));
+  res.sendFile(path.join(__dirname, './experience.html'));
 });
 
 // Page formation
 router.get('/formation', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/html/formation.html'));
+  res.sendFile(path.join(__dirname, './formation.html'));
 });
 
 
